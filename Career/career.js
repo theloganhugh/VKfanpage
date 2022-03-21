bf7fa277eda07c71536ae9094c1623f8eaca19ab
theloganhugh <63392656+theloganhugh@users.noreply.github.com>
2022-03-21 18:15:58 +0530

    Add files via upload

---
+++ Career/career.js
@@ -0,0 +1,40 @@
+const beforeDiv = document.getElementById("before");
+const afterDiv = document.getElementById("after");
+const button = document.getElementById("buttf");
+const bowlnav = document.getElementById("bowlid");
+const batnav = document.getElementById("batid");
+const capnav = document.getElementById("capid");
+const bat = document.getElementById("bat");
+const bowl = document.getElementById("bowl");
+const cap = document.getElementById("cap");
+button.onclick = function () {
+    beforeDiv.style.display = "none";
+    afterDiv.style.display = "block";
+  };
+  
+  batnav.onclick = function () {
+    bowl.style.display = "none";
+    bat.style.display = "block";
+    cap.style.display="none";
+  };
+  bowlnav.onclick = function () {
+    bowl.style.display = "block";
+    bat.style.display = "none";
+    cap.style.display="none";
+  };
+  capnav.onclick = function () {
+    bowl.style.display = "none";
+    bat.style.display = "none";
+    cap.style.display="block";
+  };
+const navBar = document.getElementById("nav");
+var lastScrollTop = 0;
+$(window).scroll(function (event) {
+  var st = $(this).scrollTop();
+  if (st > lastScrollTop) {
+    navBar.style.display = "none";
+  } else {
+    navBar.style.display = "flex";
+  }
+  lastScrollTop = st;
+});
